49eb660fcd5fb32cd736c44b68d2b4e923448774
Willard <176634778+wchutchinson@users.noreply.github.com>
2024-07-27 19:34:47 -0400

    add JavaScript to loop through values in an array and display the values to the console

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+for (let x = 0; x < topics.length; x++){
+    console.log(topics[x]);
+}
+/* First Iteration
+if (topic === "HTML"){
+    console.log("Let's Study HTML!");
+}else if (topic === "CSS"){
+    console.log("Let's Study CSS!");
+}else if (topic === "Git"){
+    console.log("Let's Study Git!");
+}else if (topic === "JavaScript"){
+    console.log("Let's Study JavaScript!");
+}else {
+    console.log("Please Try again!");
+}
+
+const shapes = ["triangle", "square", "pentagon", "circle"];
+for (let x = 0; x < shapes.length; x++) {
+    console.log(shapes[x]);
+}*/
